91024be06bd9e8d5f2e5fa9dd0a409a780c5fd6d
Joisson Mello <joisson.mello@softfocus.com.br>
2026-03-30 11:21:20 -0300

    Add ngx-markdown for code preview

--- src/app/pages/app/lesson/lesson.ts
+++ src/app/pages/app/lesson/lesson.ts
@@ -1,12 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MarkdownModule } from 'ngx-markdown';
 
 @Component({
     selector: 'app-lesson',
     standalone: true,
-    imports: [CommonModule, RouterLink],
+    imports: [CommonModule, RouterLink, MarkdownModule],
     templateUrl: './lesson.html',
     styleUrls: ['./lesson.scss']
 })
-export class Lesson {}
+export class Lesson {
+    codeSnippet = `
+\`\`\`html
+<header>
+  <nav>
+    <ul>
+      <li>Home</li>
+    </ul>
+  </nav>
+</header>
+
+<main>
+  <article>
+    <h1>O Poder da Semântica</h1>
+    <p>Conteúdo importante aqui...</p>
+  </article>
+</main>
+\`\`\`
+`;
+}
