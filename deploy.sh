bab76931379f2666530305e751b65d7db5dd1480
Joisson Mello <joisson.mello@softfocus.com.br>
2026-05-18 10:53:30 -0300

    feat: add automatic deployment steps for Supabase migrations and functions to deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,10 @@ ng build --configuration production
 echo "🚀 Deploying to Firebase..."
 firebase deploy --only hosting
 
+echo "🚀 Deploying to Supabase Migrations..."
+npx supabase db push
+
+echo "🔄 Deploying to Supabase functions..."
+npx supabase functions deploy
+
 echo "✅ Deploy complete!"
